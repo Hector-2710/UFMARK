02e52fc0cb8ddc39adef56a4ab32a33d57c99e62
HectorRosales <h.rosales01@ufromail.cl>
2024-09-19 15:53:05 -0300

    Inicio de sesion + llamando el menu y llamando el menu (platos) dependiendo la opcion del cliente.

--- UFmark/src/Main.java
+++ UFmark/src/Main.java
@@ -11,7 +11,6 @@ public class Main {
         String[] users = {"caja", "myha", "joaquin03"};
         String[] passwords = {"caja", "hola", "faundez_ufromail"};
 
-
         Scanner scanner = new Scanner(System.in);
 
         run(users, passwords, scanner);
@@ -64,8 +63,8 @@ public class Main {
 
         // llamamos al menu //
 
-        //menu(scanner);
-       //int opcion = seleccionarOpcionMenu();
+        menu(scanner);
+        int opcion = seleccionarOpcionMenu();
 
 
     }
@@ -130,5 +129,95 @@ public class Main {
 
         return false;
     }
+
+    public static void menu(Scanner scanner) {
+
+        /* Mostrando el menu con las opciones disponible */
+
+        int opcion = 0;
+        do {
+            System.out.println("-----menu-----");
+            System.out.println("1.- menu barato");
+            System.out.println("2.- menu intermedio");
+            System.out.println("3.- menu caro");
+            System.out.println("4.- salir");
+            System.out.println("selecciona una opcion:");
+            opcion = seleccionarOpcionMenu();
+
+            if (validarOpcion(opcion)) {
+
+                /* Si la opcion se valida continua ejecutando la opcion del menu llamada
+                en caso de no ser validada continuara con el menu*/
+
+                ejecutarOpcion(opcion);
+            }
+        } while ( opcion != 4);
+    }
+
+    public static int seleccionarOpcionMenu() {
+
+        // Lee un valor entero ingresado por el usuario //
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static boolean validarOpcion(int opcion) {
+
+        /* Valida que la opcion ingresada sea alguna de las que permite
+        el menu y retorna un boleano dependiendo de la validacion */
+
+        if (0 < opcion && opcion < 5) {
+            return true;
+        } else {
+            System.out.println("opcion no valida");
+            return false;
+        }
+    }
+
+    public static void ejecutarOpcion(int opcion){
+
+        /* Recibe la opcion en caso de ya ser validada y ejecuta para
+        cada opcion solicitada un desplege del menu */
+
+        switch (opcion){
+            case 1:
+                menuBarato();
+                 break;
+            case 2:
+                menuIntermedio();
+                break;
+            case 3:
+                menuCaro();
+                break;
+            case 4:
+                System.out.println("saliendo del menuu");
+                break;
+        default:
+                System.out.println("Opción no válida.");
+        }
+    }
+
+    public static void menuBarato() {
+        System.out.println("Plato principal : porotos");
+        System.out.println("Postre : banana");
+        System.out.println("Bebestible : agua");
+
+    }
+
+    public static void menuIntermedio() {
+        System.out.println("Plato principal : arroz con pollo");
+        System.out.println("Postre : chocolate");
+        System.out.println("Bebestible : bebida");
+    }
+
+    public static void menuCaro() {
+        System.out.println("Plato principal : piza");
+        System.out.println("Postre : pie de limon");
+        System.out.println("Bebestible : cafe");
+    }
+
+
+
 
 }
